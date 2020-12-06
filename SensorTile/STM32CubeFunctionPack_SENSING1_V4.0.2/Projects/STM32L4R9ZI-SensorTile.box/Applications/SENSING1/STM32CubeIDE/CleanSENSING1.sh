55c6bb9dab0e6faaa3eaed840fcb169f89b5988d
MahendraSondagar <mahendrasondagar08@gmail.com>
2020-12-06 16:14:12 +0530

    SensorTile & Box From STMicroelectronics with xcubeAI

---
+++ SensorTile/STM32CubeFunctionPack_SENSING1_V4.0.2/Projects/STM32L4R9ZI-SensorTile.box/Applications/SENSING1/STM32CubeIDE/CleanSENSING1.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+######## Modify this Section:
+# 1) Set the Installation path for OpenOCD
+# example:
+#OpenOCD_DIR="C:/Ac6/SystemWorkbench/plugins/fr.ac6.mcu.externaltools.openocd.win32_1.23.0.201904120827/tools/openocd"
+OpenOCD_DIR=""
+
+# 2) Set the installation path for stm32 OpenOCD scritps
+# example:
+#OpenOCD_CFC="C:/Ac6/SystemWorkbench/plugins/fr.ac6.mcu.debug_2.5.0.201904120827/resources/openocd/st_scripts"
+OpenOCD_CFC=""
+
+# 3) Only for Linux/iOS add openocd library path to _LIBRARY_PATH:
+# For iOS example:
+#export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${OpenOCD_DIR}"lib/"
+
+# For Linux example:
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OpenOCD_DIR}"lib/"
+
+######## Don't change the following part
+
+## Control Section
+
+if [[ ! $OpenOCD_DIR ]];  then
+	echo "Please add the rigth path to OpenOCD_DIR Variable"
+	exit
+fi
+
+if [[ ! $OpenOCD_CFC ]];  then
+	echo "Please add the rigth path to OpenOCD_CFC Variable"
+	exit
+fi
+
+
+## Run section
+
+# OpenOCD command
+OpenOCD_CMD="${OpenOCD_DIR}/bin/openocd -s ${OpenOCD_CFC} -f STM32L4R9I-SensorTileBox.cfg"
+
+
+echo "/******************************************/"
+echo "           Clean FP-AI-SENSING1"
+echo "/******************************************/"
+echo "              Full Chip Erase"
+echo "/******************************************/"
+${OpenOCD_CMD} -c "init" -c "reset halt" -c "flash erase_sector 0 511 511" -c "shutdown"
+echo "/******************************************/"
+echo "              Install BootLoader"
+echo "/******************************************/"
+${OpenOCD_CMD} -c "init" -c "reset halt" -c "flash write_image erase ../../../../../Utilities/BootLoader/STM32L4R9ZI/BootLoaderL4R9.bin 0x08000000 bin" -c "shutdown"
+echo "/******************************************/"
+echo "          Install FP-AI-SENSING1"
+echo "/******************************************/"
+${OpenOCD_CMD} -c "init" -c "reset halt" -c "flash write_image erase  ./SENSING1.bin 0x08004000 bin" -c "shutdown"
+echo "/******************************************/"
+echo "      Dump FP-AI-SENSING1 + BootLoader"
+echo "/******************************************/"
+
+SizeBinBL=`ls -l ./SENSING1.bin | awk '{print $5+0x4000};'`
+${OpenOCD_CMD} -c "init" \
+			   -c "reset halt" \
+			   -c "dump_image ./SENSING1_BL.bin 0x08000000 ${SizeBinBL}" \
+			   -c "shutdown"
+
